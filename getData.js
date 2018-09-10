c899afbb9d0e1660d8552a6a58a93cc3362ede6a
Oscar Wieman <oscar@gofilex.nl>
2018-09-10 20:31:14 +0200

    living on the edge

--- getData.js
+++ getData.js
@@ -3,7 +3,7 @@ let winkel = 'https://www.biernet.nl/bier/aanbiedingen/bij/albert-heijn'
 
 
 let scrape = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
     await page.goto(winkel);
     await page.waitFor(1000);
@@ -12,14 +12,14 @@ let scrape = async () => {
         let data = [];
         let aanbiedingen = document.getElementsByClassName('textaanbieding');
 
-        for (let aanbieding of aanbiedingen){ 
+        for (let aanbieding of aanbiedingen){
             let merk  = aanbieding.getElementsByClassName('merk')[0].innerText;
             let prijsOud = aanbieding.getElementsByClassName('prijsboven')[0].innerText.split("\n")[0];
             let prijsNieuw = aanbieding.getElementsByClassName('prijsboven')[0].innerText.split("\n")[1];
             let hoeveelheid = aanbieding.querySelectorAll('.Blikjes, .Flessen, .Kratten')[0].innerText;
             let geldigheid = aanbieding.getElementsByClassName('nomargin')[0].innerText;
-            
-            data.push({merk, prijsOud, prijsNieuw, hoeveelheid, geldigheid}); 
+
+            data.push({merk, prijsOud, prijsNieuw, hoeveelheid, geldigheid});
 
         }
         return {
@@ -31,9 +31,7 @@ let scrape = async () => {
     browser.close();
     return result
   };
-  
+
   scrape().then((value) => {
-      console.log(value); 
+      console.log(value);
   });
-
-
