48cd40a5b80b7f7af8e4ddb99885ad7dcc599b99
nvdlaan <nino.vanderlaan@hotmail.com>
2018-09-10 15:24:18 +0200

    Beginnetje

---
+++ getData.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+let winkel = 'https://www.biernet.nl/bier/aanbiedingen/bij/albert-heijn'
+
+
+let scrape = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(winkel);
+    await page.waitFor(1000);
+
+    const result = await page.evaluate(() => {
+        let data = [];
+        let aanbiedingen = document.getElementsByClassName('textaanbieding');
+
+        for (let aanbieding of aanbiedingen){ 
+            let merk  = aanbieding.getElementsByClassName('merk')[0].innerText;
+            let prijsOud = aanbieding.getElementsByClassName('prijsboven')[0].innerText.split("\n")[0];
+            let prijsNieuw = aanbieding.getElementsByClassName('prijsboven')[0].innerText.split("\n")[1];
+            let hoeveelheid = aanbieding.querySelectorAll('.Blikjes, .Flessen, .Kratten')[0].innerText;
+            let geldigheid = aanbieding.getElementsByClassName('nomargin')[0].innerText;
+            
+            data.push({merk, prijsOud, prijsNieuw, hoeveelheid, geldigheid}); 
+
+        }
+        return {
+            data
+        }
+
+    });
+
+    browser.close();
+    return result
+  };
+  
+  scrape().then((value) => {
+      console.log(value); 
+  });
+
+
