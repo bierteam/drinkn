baed59271e000fd2b262af0e8cc9aa221509f202
nvdlaan <nino.vanderlaan@hotmail.com>
2018-10-29 20:30:38 +0100

    Added devmode
    Disables authentication when enabled

--- routes/requiresLogin.js
+++ routes/requiresLogin.js
@@ -1,9 +1,15 @@
+const config = require('./../config')
+const devmode = config.app.devmode
+
 const requiresLogin = (req, res, next) => {
-  if (req.session && req.session.userId) {
-    return next()
+  if (!devmode) {
+    if (req.session && req.session.userId) {
+      return next()
+    } else {
+      return res.redirect('/login')
+    }
   } else {
-    return res.redirect('/login')
+    return next()
   }
 }
-
 module.exports = requiresLogin
