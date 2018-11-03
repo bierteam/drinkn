6d129c520957c8583c46aa5e78264fb6317df1fb
Oscar Wieman <oscar@gofilex.nl>
2018-11-03 15:24:12 +0100

    add uri prettyfier

--- routes/processData.js
+++ routes/processData.js
@@ -1,7 +1,13 @@
 const storeMapping = require('./storeMapping')
+const uriPrettifier = require('./uriPrettifier')
 const processData = (data) => {
   for (let obj in data) {
     data[obj].store = storeMapping(data[obj].store)
+
+    if (data[obj].rawUri) {
+      console.log(uriPrettifier(data[obj].rawUri))
+      data[obj].rawUri = uriPrettifier(data[obj].rawUri)
+    }
   }
   return data
 }
---
+++ routes/uriPrettifier.js
@@ -0,0 +1,21 @@
+const uriPrettifier = (rawUri) => {
+  let uri
+  uri = rawUri
+    .replace(/(.*)tion:/g, '')
+    .replace(/%2F/g, '/')
+    .replace(/%3A/g, ':')
+    .replace(/(.*)diurl=/g, '')
+    .replace(/(.*)diurl%3D/g, '')
+    .replace(/\?utm_source(.*)/g, '')
+    .replace(/%3Freturn(.*)/g, '')
+    .replace(/\?return(.*)/g, '')
+    .replace(/(.*)&p=/g, '')
+    .replace(/http[^s]/g, 'https:')
+    .replace(/%253A/g, ':')
+    .replace(/%252F/g, '/')
+    .replace(/%23tab2(.*)/g, '')
+
+  return uri
+}
+
+module.exports = uriPrettifier
