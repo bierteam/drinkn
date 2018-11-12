5654b441ef40c0ca5edc0a6f8bef603ba64630bd
nvdlaan <nino.vanderlaan@hotmail.com>
2018-11-12 21:13:10 +0100

    Update to scrape rebuild

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,43 +1,37 @@
-const puppeteer = require('puppeteer')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
 const config = require('./../config')
 
-const scrape = async () => {
-  console.log('Launching browser')
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
-  const page = await browser.newPage()
-  console.log('Loading page')
-  await page.goto(config.scraper.uri)
-  const result = await page.evaluate(() => {
-    let data = []
-    let aanbiedingen = document.getElementsByClassName('textaanbieding')
+const scrape = () => {
+  rp(config.scraper.uri)
+    .then(function (html) {
+      const $ = cheerio.load(html)
+      let aanbiedingen = $('div.textaanbieding')
+      let data = []
+      aanbiedingen.each(function () {
+        let brand = $(this).find('span.merk').text()
+        if (brand.includes('0.0')) {
+          return true // It's not beer when there is no alcohol in it
+        }
+        let store = $(this).find('img')[0].attribs.title
+        let oldPrice = $(this).find('del').text()
+        let newPrice = $(this).find('span.prijs').text()
+        let volume = $(this).find('.Blikjes, .Flessen, .Kratten, .Fusten').text()
+        let rawValidity = $(this).find('p:nth-child(1)').text().trim()
+        let rawUri
 
-    for (let aanbieding of aanbiedingen) {
-      let brand = aanbieding.getElementsByClassName('merk')[0].innerText
-      if (brand.includes('0.0')) {
-        continue // It's not beer when there is no alcohol in it
-      }
-      let store = aanbieding.querySelector('div.textaanbieding > div.fotowinkel > a > img').title
-      let oldPrice = aanbieding.getElementsByClassName('prijsboven')[0].innerText.split('\n')[0]
-      let newPrice = aanbieding.getElementsByClassName('prijs')[0].innerText
-      let volume = aanbieding.querySelectorAll('.Blikjes, .Flessen, .Kratten, .Fusten')[0].innerText
-      let rawValidity = aanbieding.getElementsByClassName('nomargin')[0].innerText
-      let rawUri
+        if ($(this).find('a.button.yellow.aanbtn').length > 0) {
+          rawUri = $(this).find('a.button.yellow.aanbtn')[0].attribs.href
+        }
 
-      if (aanbieding.querySelector('div.textaanbieding > a.button.yellow.aanbtn')) {
-        rawUri = aanbieding.querySelector('div.textaanbieding > a.button.yellow.aanbtn').href
-      }
-
-      if (rawUri) {
-        data.push({ brand, store, oldPrice, newPrice, volume, rawUri, rawValidity })
-      } else {
-        data.push({ brand, store, oldPrice, newPrice, volume, rawValidity })
-      }
-    }
-    return { data }
-  })
-  console.log('Succesfully scraped data')
-  await browser.close()
-  return result.data
+        if (rawUri) {
+          data.push({ brand, store, oldPrice, newPrice, volume, rawUri, rawValidity })
+        } else {
+          data.push({ brand, store, oldPrice, newPrice, volume, rawValidity })
+        }
+      })
+      return data
+    })
 }
 
 module.exports = scrape
