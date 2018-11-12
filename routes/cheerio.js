93833b6e94f7a7f179b0b0e5b7249ae115126f82
oscrx <oscar@oscarr.nl>
2018-11-12 18:17:31 +0100

    bit of improvement
    Co-Authored-By: nvdlaan <nvdlaan@users.noreply.github.com>

--- routes/cheerio.js
+++ routes/cheerio.js
@@ -1,19 +1,30 @@
 const rp = require('request-promise')
 const cheerio = require('cheerio')
 const config = require('./../config')
+var fs = require('fs')
 
-const url = config.scraper.uri
-rp(url)
+rp(config.scraper.uri)
   .then(function (html) {
     const $ = cheerio.load(html)
     let aanbiedingen = $('div.textaanbieding')
     let data = []
+    const newPrices = $('span.prijs')
+    const oldPrices = $('del')
+    const stores = $('div.fotowinkel > a > img')
+    const volumes = $('.Blikjes, .Flessen, .Kratten, .Fusten')
     for (let i = 0; i < aanbiedingen.length; i++) {
-      let store = ($('div.fotowinkel > a > img')[i].attribs.title)
-      let oldPrice = $('h2 > a > span')[i].children[0].data
-      let newPrice = $('span.prijs')[i].children[0].data
-      let volume = $('.Blikjes, .Flessen, .Kratten, .Fusten')[i].children[0].data
+      let store = stores[i].attribs.title
+      let oldPrice = oldPrices[i].children[0].data
+      let newPrice = newPrices[i].children[0].data
+      let volume = volumes[i].children[0].data
       data.push({ store, oldPrice, newPrice, volume })
     }
-    console.log(data)
+    fs.writeFile('cdata.json', JSON.stringify(data),
+      function (err) {
+        if (err) throw err
+        console.log('data is written to file successfully.')
+      })
+  })
+  .catch(function (err) {
+    console.error(err)
   })
