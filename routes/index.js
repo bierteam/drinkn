743b280636a9d07b3920cccfd5d0f4a136dbb19a
nvdlaan <nino.vanderlaan@hotmail.com>
2018-12-07 17:48:22 +0100

    Added example on how to get data for single store

--- routes/index.js
+++ routes/index.js
@@ -27,25 +27,26 @@ router.post('/', requiresLogin, function (req, res) {
   res.render('home')
 })
 
-// router.get('/aanbiedingen', requiresLogin, function (req, res) {
-//   counter.findOne({}).exec(function (err, result) {
-//     batch = result.counter
-//     if (err) throw err
-//   })
-//   const storeQuery = beer.find({}).distinct('store')
-//   storeQuery.exec(function (err, result) {
-//     stores = result
-//     if (err) throw err
-//     res.render('aanbiedingen', { storeDataResponse: stores, pilsDataResponse: '' })
-//   })
-// })
-
 router.get('/aanbiedingen', function (req, res) {
   counter.findOne({}).exec(function (err, result) {
     batch = result.counter
     if (err) throw err
   })
-  let query = beer.find({ batch }).limit(5) // limit on 5 for testing purposes
+  let query = beer.find({ batch })
+  query.exec(function (err, results) {
+    if (err) throw err
+    res.json(results)
+  })
+})
+
+// Example on how to get data for specific store
+router.get('/aanbiedingen/:store', function (req, res) {
+  let store = req.params.store
+  counter.findOne({}).exec(function (err, result) {
+    batch = result.counter
+    if (err) throw err
+  })
+  let query = beer.find({ batch, store }) // .limit(5) // limit on 5 for testing purposes
   query.exec(function (err, results) {
     if (err) throw err
     res.json(results)
