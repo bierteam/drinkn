d8412533df76d66db6423d733c5674b477b24f90
Oscar Wieman <oscrx@icloud.com>
2018-12-25 11:30:39 +0100

    add level items

--- src/components/Aanbiedingen.vue
+++ src/components/Aanbiedingen.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="aanbiedingen">
-    <h1 class="title">Aanbiedingen</h1>
-    <div v-for="aanbieding in aanbiedingen" :key="aanbieding.id">
-      <p>
-        <span><b>{{ aanbieding.brand }}</b></span><br />
-      </p>
+  <div>
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Aanbiedingen</p>
+          <p class="title">3,456</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Gemmiddelde korting</p>
+          <p class="title">123</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Online aanbiedingen</p>
+          <p class="title">456K</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Nog een vakje</p>
+          <p class="title">789</p>
+        </div>
+      </div>
+    </nav>
+    <div class="aanbiedingen">
+      <h1 class="title">Aanbiedingen</h1>
+      <div v-for="aanbieding in aanbiedingen" :key="aanbieding.id">
+        <p>
+          <span><b>{{ aanbieding.brand }}</b></span><br />
+        </p>
+      </div>
     </div>
   </div>
 </template>
