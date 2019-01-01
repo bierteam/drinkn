64a4ecfe2a3ecc272bb3587352e38e68bfb1e2e4
Oscar Wieman <oscar@oscarr.nl>
2019-01-01 18:43:23 +0100

    improve performance + cleanup

--- api/v1/index.js
+++ api/v1/index.js
@@ -5,6 +5,7 @@ const store = require('../../models/store')
 const counter = require('../../models/counter')
 const dbImport = require('../../methods/dbImport')
 
+// load and maintain counter in memory
 let batch
 counter.findOne().exec(function (err, result) {
   if (err) console.error(err)
@@ -14,18 +15,32 @@ counter.watch().on('change', function (data) {
   batch = data.updateDescription.updatedFields.counter
 })
 
+// load and maintain beer discounts in memory
+let aanbiedingen
+function waitForBatch () {
+  if (typeof batch !== 'undefined') {
+    beer.find({ batch }).exec(function (err, result) {
+      if (err) console.error(err)
+      aanbiedingen = result
+    })
+  } else {
+    setTimeout(waitForBatch, 250)
+  }
+}
+waitForBatch()
+
+beer.watch().on('change', function (data) {
+  aanbiedingen = data.updateDescription.updatedFields.counter
+})
+
 router.get('/aanbiedingen', function (req, res) {
-  let query = beer.find({ batch }) // .limit(5) // limit on 5 for testing purposes
-  query.exec(function (err, results) {
-    if (err) throw err
-    res.json(results)
-  })
+  res.json(aanbiedingen)
 })
 
 // Example on how to get data for specific store
 router.get('/aanbiedingen:store', function (req, res) {
   let store = req.params.store
-  let query = beer.find({ batch, store }) // .limit(5) // limit on 5 for testing purposes
+  let query = beer.find({ batch, store })
   query.exec(function (err, results) {
     if (err) throw err
     res.json(results)
@@ -34,8 +49,25 @@ router.get('/aanbiedingen:store', function (req, res) {
 
 router.get('/stores', function (req, res) {
   store.findOne({}).exec(function (err, result) {
-    if (err) throw err
+    if (err) console.error(err)
     res.json(result)
+  })
+})
+
+// DEBUG { $set: req.body.newStores }
+// DEBUG { $set: {"test" : "test"} }
+router.post('/stores', function (req, res) {
+  store.findOneAndUpdate({}, { $set: req.body.newStores }, { strict: false, new: true }, function (err, result) {
+    if (err) console.error(err)
+    res.json(result)
+  })
+})
+
+router.delete('/stores', function (req, res) {
+  console.log(req.body)
+  store.updateOne({}, { $unset: req.body.remove }, { strict: false }, function (err, result) {
+    if (err) console.error(err)
+    res.json(result.ok)
   })
 })
 
