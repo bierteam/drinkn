ec8395f817912ab7b2a2eb9bf1f356713285efaf
Oscar Wieman <oscar@oscarr.nl>
2019-01-04 17:03:41 +0100

    implement menu logic

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
           <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
         </a> -->
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" class="navbar-burger burger" @click="burger = !burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" v-bind:class="{'is-active': burger}">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div id="navbarBasicExample" class="navbar-menu" v-bind:class="{'is-active': burger}">
         <div class="navbar-start">
           <router-link class="navbar-item" to="/">Home</router-link>
           <router-link class="navbar-item" to="/aanbiedingen">Aanbiedingen</router-link>
@@ -77,7 +77,27 @@
 </template>
 
 <script>
+// addToHomescreen();
+
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      burger: false
+    }
+  }
 }
+
 </script>
+
+<style>
+/* .wrapper { min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -30px; }
+.footer, #push { height:20px;} */
+/* .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    } */
+</style>
