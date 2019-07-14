e1b0c2b31050432780192eff61bc65bf19cf98ad
Oscar Wieman <oscar@oscarr.nl>
2019-07-14 20:43:29 +0200

    Update Api.js
    add litte help config

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,6 +4,8 @@ export default() => {
   if (process.env.NODE_ENV === 'development') {
     return axios.create({
       baseURL: `http://localhost:3000`
+      // for mobile phone testing in local network:
+      // baseURL: `http://192.168.123.123:3000`
     })
   } else {
     return axios.create({
