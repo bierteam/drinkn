d91772aee09de90a9c99a2031276070ffb3326ca
Oscar Wieman <oscar@oscarr.nl>
2019-07-20 21:50:07 +0200

    clean up the received data

--- methods/dbImport.js
+++ methods/dbImport.js
@@ -1,18 +1,32 @@
-const requestData = require('./requestData')
-const joinObjects = require('./joinObjects')
-
-const endPoints = ['soort', 'winkel', 'inhoud', 'inhoud_soort', 'land', 'merken', 'merken_soort', 'nieuws', 'plaats', 'provincie', 'agenda', 'bierkoerier', 'brouwerij', 'gisting', 'home', 'set_next_open']
+const getData = require('./getData')
+const store = require('../models/store')
+const processData = require('./processData')
+const beer = require('../models/beer')
 
 const dbImport = async () => {
-  let aanbiedingen = await requestData('aanbieding')
-  for (let endPoint in endPoints) {
-    let currentResponse = await requestData(endPoints[endPoint])
-    let currentEndPoint = endPoints[endPoint]
-    let currentUid = currentEndPoint + '_uid'
-    aanbiedingen = await joinObjects(aanbiedingen, currentResponse, currentUid, 'uid', currentEndPoint)
-    // console.log(currentResponse)
+  let data, stores
+  data = await getData()
+  let storeQuery = store.findOne({}, { '_id': false })
+  let storeExec = storeQuery.exec()
+  let result = await storeExec
+  if (result && result._doc) {
+    stores = result._doc
   }
-  console.log(aanbiedingen)
+  console.log('Attemping to process data...')
+  let processedData = await processData(data, stores)
+  console.log('Succesfully processed data')
+  console.log('Importing to database...')
+  for (let obj of processedData) {
+    let search = { id: obj.id }
+    beer.find(search, function (err, result) {
+      if (err) console.error(err)
+      if (result.length < 1) {
+        beer.create(obj, function (err) {
+          if (err) console.error(err)
+        })
+      }
+    })
+  }
 }
 
 module.exports = dbImport
--- methods/processData.js
+++ methods/processData.js
@@ -1,5 +1,4 @@
 const uriPrettifier = require('./uriPrettifier')
-const prettyPrice = require('./prettyPrice')
 const updateStores = require('./updateStores')
 const revHash = require('rev-hash')
 const moment = require('moment')
@@ -14,39 +13,57 @@ const processData = (data, stores) => {
   }
 
   for (let obj in data) {
+    // console.log(data[obj])
+    // if (!data[obj].merken_name) {
+    //   console.log(data[obj])
+    // }
+
+    data[obj].store = data[obj].winkel_name
+    data[obj].brand = data[obj].merken_name
+    data[obj].pricing = {}
+    data[obj].pricing.rawOldPrice = data[obj].vanprijs
+    data[obj].pricing.rawNewPrice = data[obj].voorprijs
+    data[obj].volume = data[obj].korte_name
+    data[obj].rawUri = data[obj].aanbieding_link
+    data[obj].rawValidity = data[obj].einddatum
+    delete data[obj].merken_soort_omschrijving
+    delete data[obj].brouwerij_omschrijving
+    delete data[obj].gisting_omschrijving
+
+    if (data[obj].alcoholpercentage) {
+      data[obj].rawAlcoholpercentage = data[obj].alcoholpercentage
+      data[obj].rawAlcoholpercentage = parseFloat(data[obj].rawAlcoholpercentage).toFixed(2)
+      data[obj].alcoholpercentage = data[obj].rawAlcoholpercentage * 100
+    }
+    if (data[obj].kleur) {
+      data[obj].color = data[obj].kleur
+    }
+    if (data[obj].aantal_liter) {
+      data[obj].rawLiter = data[obj].aantal_liter
+      data[obj].liter = data[obj].rawLiter * 1000
+    }
     if (!newStores[data[obj].store]) {
       newStores[data[obj].store] = data[obj].store
     }
-
     if (storeMap && storeMap.get(data[obj].store)) {
       data[obj].rawStore = data[obj].store
       data[obj].store = storeMap.get(data[obj].store)
     }
-
-    data[obj].id = revHash(data[obj].id)
-
-    let validity = data[obj].rawValidity.replace(/(.*)t\/m /g, '')
-
-    if (moment(validity, 'dddd DD MMMM').isValid()) {
-      data[obj].validity = moment(validity, 'dddd DD MMMM').add(1, 'days').toDate()
+    if (moment(data[obj].rawValidity, 'dddd DD MMMM').isValid()) {
+      data[obj].validity = moment(data[obj].rawValidity, 'dddd DD MMMM').add(1, 'days').toDate()
     }
-
-    data[obj].importDate = moment().toDate()
-
-    let tmp = {}
-    tmp.oldPrice = prettyPrice(data[obj].pricing.rawOldPrice)
-    tmp.newPrice = prettyPrice(data[obj].pricing.rawNewPrice)
-    tmp.literPrice = prettyPrice(data[obj].pricing.rawLiterPrice)
-
-    data[obj].pricing.oldPrice = tmp.oldPrice * 100
-    data[obj].pricing.newPrice = tmp.newPrice * 100
-    data[obj].pricing.literPrice = tmp.literPrice * 100
-
     if (data[obj].rawUri) {
       data[obj].uri = uriPrettifier(data[obj].rawUri)
     }
+
+    data[obj].id = revHash(data[obj].uid)
+    data[obj].importDate = moment().toDate()
+    data[obj].pricing.oldPrice = data[obj].pricing.rawOldPrice * 100
+    data[obj].pricing.newPrice = data[obj].pricing.rawNewPrice * 100
+    data[obj].pricing.literPrice = data[obj].rawLiter / data[obj].pricing.newPrice
   }
   updateStores(newStores)
+  // console.log(data)
   return data
 }
 
