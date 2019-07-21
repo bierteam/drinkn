6240452abf79bc2ed7d70d1c52b59bd27981f0a1
Oscar Wieman <oscar@oscarr.nl>
2019-07-21 15:16:25 +0200

    cleanup

--- test/unit/methods/joinObjects.test.js
+++ test/unit/methods/joinObjects.test.js
@@ -1,27 +1,27 @@
 /* global test, expect */
 const joinObjects = require('../../../methods/joinObjects')
 
-let obj1 = [{
-  'uid': '123',
-  'name': 'test'
+const obj1 = [{
+  uid: '123',
+  name: 'test'
 },
 {
-  'uid': '1234',
-  'name': 'test2'
+  uid: '1234',
+  name: 'test2'
 }
 ]
 
-let obj2 = [{
-  'uid2': '123',
-  'name': 'test3'
+const obj2 = [{
+  uid2: '123',
+  name: 'test3'
 },
 {
-  'uid2': '1234',
-  'name2': 'test4'
+  uid2: '1234',
+  name2: 'test4'
 }
 ]
 
-const expectedResult = [ { uid: '123', name: 'test', uid2: '123', test_name: 'test3' }, { uid: '1234', name: 'test2', uid2: '1234', name2: 'test4' } ]
+const expectedResult = [{ uid: '123', name: 'test', uid2: '123', test_name: 'test3' }, { uid: '1234', name: 'test2', uid2: '1234', name2: 'test4' }]
 
 test('Expect 2 objects to join correctly', () => {
   expect(joinObjects(obj1, obj2, 'uid', 'uid2', 'test')
