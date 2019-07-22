ce360f9f3cd61d0eff4a76a35bfaca947ea66aa7
Oscar Wieman <oscar@oscarr.nl>
2019-07-22 17:45:44 +0200

    outdated browser notification

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueBrowserUpdate from 'vue-browserupdate'
 import './../node_modules/bulma/css/bulma.css'
 import './../node_modules/bulma-tooltip/dist/css/bulma-tooltip.min.css'
 // import './../node_modules/@fortawesome/fontawesome-free/css/all.css'
@@ -15,4 +16,19 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+Vue.use(VueBrowserUpdate, {
+  required: {
+    e: -2,
+    f: -2,
+    o: -2,
+    s: -2,
+    c: -2
+  },
+  options: {
+    insecure: true,
+    unsupported: true
+  }
+  // test: true
 })
