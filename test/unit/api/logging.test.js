496876d00390bb690726f98035f92d6aa0a7abfc
NvdLaan <34895069+NvdLaan@users.noreply.github.com>
2019-07-23 21:09:52 +0200

    Revert "First api test"
    This reverts commit 7730680c3f1ecb3455c401f1eb68b188fea75a6a.

--- test/unit/api/logging.test.js
+++
@@ -1,9 +0,0 @@
-/* global test, expect */
-
-const request = require('supertest')
-const app = require('../../../app')
-test('It should response the GET method', async (done) => {
-  const response = await request(app).get('/api/v1/logging/logs')
-  expect(response.statusCode).toBe(200)
-  done()
-})
