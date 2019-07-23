f420ddbba3c45fcdfe28098b2185b60c571d6f33
NvdLaan <34895069+NvdLaan@users.noreply.github.com>
2019-07-23 22:13:39 +0200

    Removed last endpoints from index.js

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,43 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const store = require('../../models/store')
-const dbImport = require('../../services/dbImport')
-const isAuthenticated = require('../../services/isAuthenticated')
 const aanbieding = require('./aanbieding')
+const store = require('./stores')
 
 const users = require('./users')
 const logging = require('./logging')
+const dbImport = require('./dbImport')
+
 router.use('/users', users)
 router.use('/logging', logging)
 router.use(aanbieding)
-
-router.get('/stores', isAuthenticated, function (req, res) {
-  store.findOne({}).exec(function (err, result) {
-    if (err) console.error(err)
-    res.json(result)
-  })
-})
-
-// DEBUG { $set: req.body.newStores }
-// DEBUG { $set: {"test" : "test"} }
-router.post('/stores', isAuthenticated, function (req, res) {
-  store.findOneAndUpdate({}, { $set: req.body.newStores }, { strict: false, new: true }, function (err, result) {
-    if (err) console.error(err)
-    res.json(result)
-  })
-})
-
-router.delete('/stores', isAuthenticated, function (req, res) { // WIP
-  console.log(req.body)
-  store.updateOne({}, { $unset: req.body.remove }, { strict: false }, function (err, result) {
-    if (err) console.error(err)
-    res.json(result.ok)
-  })
-})
-
-router.post('/import', isAuthenticated, function (req, res) {
-  dbImport()
-  res.json('received')
-})
+router.use(store)
+router.use(dbImport)
 
 module.exports = router
