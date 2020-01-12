5aeec9aa69445665e58253484f4f4feba83e5ea5
Oscar Wieman <oscar@oscarr.nl>
2020-01-12 16:23:14 +0100

    split up pils v1

--- pils-legacy/src/components/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- <figure class="image is-16by9">
-    <img :src="`//media.disquscdn.com/errors/img/${Math.ceil( Math.random() * 8 )}.gif`">
-  </figure> -->
-  <figure class="image is-16by9">
-    <img :src="`//media.disquscdn.com/errors/img/${3}.gif`">
-  </figure>
-</template>
