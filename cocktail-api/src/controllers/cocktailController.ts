b36dcd2d813098848f39d86e99ada4b9d86f0768
Oscar Wieman <oscar@oscarr.nl>
2020-01-25 17:30:46 +0100

    Update cocktailController.ts

--- cocktail-api/src/controllers/cocktailController.ts
+++ cocktail-api/src/controllers/cocktailController.ts
@@ -3,17 +3,10 @@ import { Request, Response } from 'express'
 import { Cocktail, ICocktail } from '../models/cocktail'
 
 export class CocktailController {
-<<<<<<< HEAD
-  public async getCocktails (req: Request, res: Response): Promise<void> {
-    const cocktails = await Cocktail.find()
-    console.log(cocktails)
-    res.json(cocktails)
-=======
 
   public async getCocktails(req: Request, res: Response): Promise<void> {
     const cocktails = await Cocktail.find();
     res.json(cocktails);
->>>>>>> dc16421936b66114f167288b5cd3ea2c35f25e0f
   }
 
   public async getCocktail (req: Request, res: Response): Promise<void> {
