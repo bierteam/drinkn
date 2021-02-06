ed11b7768105c6c63ce6f50ab3844405f76e468b
Oscar Wieman <oscar@oscarr.nl>
2021-02-07 00:39:30 +0100

    fix check endpoint

--- auth/routes/check.js
+++ auth/routes/check.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
 // This tells the client their login status
-
-router.get('/', function (req, res) {
-  if (req) {
-    res.send(true)
-  } else {
-    res.send(false)
+router.get('/', auth, async (req, res) => {
+  try {
+    if (req.user) {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (error) {
+    res.status(500).send()
   }
 })
 
