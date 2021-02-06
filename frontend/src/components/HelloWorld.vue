efcd10031b4b4a76c309ad5f277b0f2ac15210d6
Oscar Wieman <oscar@oscarr.nl>
2021-02-07 00:42:51 +0100

    implement jwt flow

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <b-alert v-if="message" show variant="info" dismissible>{{message}}</b-alert>
+    <b-alert v-if="error" show variant="danger" dismissible>{{error}}</b-alert>
+    <button type="button" class="btn btn-warning " @click="resetJWT">Delete jwt</button>
+    <button type="button" class="btn btn-primary " @click="check">Check login</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,12 +35,38 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import Api from '@/services/Api'
+
+  export default {
+    data () { // local data
+      return {
+        error: '',
+        message: ''
+      }
+    },
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    methods: {
+      resetJWT () {
+        this.$store.commit('saveJWT', "")
+        this.$data.message = 'Deleted JWT'
+        console.log('Deleted JWT')
+      },
+      check () {
+        Api().get('auth/check')
+          .then(response => {
+            console.log(response.status)
+            this.$data.message = response.status
+          })
+          .catch(e => {
+            this.$data.error = e.response.data || e
+            console.error(e)
+          })
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
-import VueCookies from 'vue-cookies'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue, VueCookies)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
