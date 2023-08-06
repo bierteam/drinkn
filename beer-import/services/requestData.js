2f743da6c5e9303cc483c4ff0ff0afff30774b36
Oscar Wieman <oscar@oscarr.nl>
2023-08-06 22:45:33 +0200

    remove request(promise)

--- beer-import/services/requestData.js
+++ beer-import/services/requestData.js
@@ -1,19 +1,23 @@
-const rp = require('request-promise')
+const axios = require('axios')
 
 const requestData = async (context) => {
-  const options = { // move to envirioment variable?
-    uri: `https://biernet.nl/extra/app/V3_3.3.4/${context}.php?d=`,
-    headers: {
-      'User-Agent': 'nl.Biernet.iOS.app/V3',
-      Accept: 'application/json, text/plain, /',
-      'Accept-Language': 'nl-nl',
-      Connection: 'close'
-      // TODO implement: 'Accept-Encoding': 'gzip, deflate'
-    },
-    json: true // Automatically parses the JSON string in the response
+  const headers = {
+    'User-Agent': 'nl.Biernet.iOS.app/V3',
+    Accept: 'application/json, text/plain, /',
+    'Accept-Language': 'nl-nl',
+    Connection: 'close',
+    'Accept-Encoding': 'gzip, deflate'
   }
-  const response = await rp(options)
-  return response
+
+  const url = `https://biernet.nl/extra/app/V3_3.3.4/${context}.php?d=`
+
+  try {
+    const response = await axios.get(url, { headers, responseType: 'json' })
+    return response.data
+  } catch (error) {
+    console.error('Error while requesting data:', error.message)
+    throw error
+  }
 }
 
 module.exports = requestData
