09847253e3c9f89f5ca8afd6ba2dce2eca36bcfd
Oscar Wieman <oscar@oscarr.nl>
2023-08-07 05:14:25 +0200

    Add local mongo

--- pils/server.js
+++ pils/server.js
@@ -1,6 +1,14 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config() // use the .env file for this
 require('./setup')
-const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+
+let mongoProtocol
+if (process.env.NO_SRV) {
+  mongoProtocol = 'mongodb'
+} else {
+  mongoProtocol = 'mongodb+srv'
+}
+
+const connectionString = `${mongoProtocol}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
 const express = require('express')
 const history = require('connect-history-api-fallback')
